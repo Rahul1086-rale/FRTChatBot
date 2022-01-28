2b0a9fc0d286bd4dc7ab580c3b17626b09d46737
Rahul1086-rale <84489118+Rahul1086-rale@users.noreply.github.com>
2022-01-28 11:22:12 +0530

    Add files via upload

---
+++ frtchatbot2-bot-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\frtchatbot2-bot-9d30-Web-Deploy.pubxml -p:Password=Jxfc6TtdixElyqTohPftXnFqb0oWopGNgtcqYRYaYN8vbNrnccXLFtybl91A
+
